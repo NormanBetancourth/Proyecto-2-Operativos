1ccc1968f9b2d05b1ce6a0ed995f35566a7b6ad8
NormanBetancourth <norman.betancourtt.mairena@est.una.ac.cr>
2022-06-26 00:03:56 -0600

    ya hace el request

--- client/main.py
+++ client/main.py
@@ -1,8 +1,20 @@
 import requests
 
 palabra =  input("Seleccione la opcion: ").lower()
-print(palabra)
 
 url = f"http://localhost:5000/traductor?palabra=${palabra}"
 res = requests.get(url)
-print(res)
+print(res.content)
+
+
+#TODO: cuando no se encuentre la respuesta se usa el not fount
+#TODO: Nota: esto debe ir en el servidor
+
+car = None
+map = {'carro':'res'}
+try:
+  car = map["a"]
+except:
+  car = 'not found'
+
+print(car)
--- server/server.py
+++ server/server.py
@@ -1,16 +1,46 @@
 from flask import Flask
 from flask import request
 
+import threading
+from threading import Thread, Lock
+
+traducciones = {
+    'carro':'car',
+    'casa':'house',
+    'caballo':'horse',
+    'silla':'chair',
+    'libro':'book',
+    }
+
+
+def thread_fuc(headers, palabra):
+    mutex.acquire()
+    try:
+        print(palabra)
+        print(headers)
+        respuesta = traducciones[palabra]
+        print('respuesta: '+respuesta)
+    finally:
+        mutex.release()
+
+    return respuesta
+    
+
+mutex = Lock()
+
 app = Flask(__name__)
 
 @app.route('/traductor')
 def get():
-    print(request.args.get('palabra'))
+    
     print(request)
-    headers = request.headers
-    print(headers)
+    x = threading.Thread(target=thread_fuc, args=(request.headers, request.args.get('palabra')[1:],))
+    x.start()
+    
+    #TODO: arbir el hilo, una vez creado agregamos al log y retornamos la respuesta
+    # return request.args.get('palabra')
+    return traducciones[request.args.get('palabra')[1:]]
 
-    return request.args.get('palabra')
 
-    #TODO: aqui es donde se debe crear el hilo y orquestar lo de los logs
+
 app.run(host='localhost', port=5000)
